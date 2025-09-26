3add060ef33f6d3be0cfa5c9bc516e59e399f4c9
Єлизавета Коль <121323125+KolYelyzaveta@users.noreply.github.com>
2025-09-26 22:24:23 +0300

    Guide Text
    Add little disapearing guide at first level

--- Assets/Skripts/UI/LevelUI/PlayerUI.cs
+++ Assets/Skripts/UI/LevelUI/PlayerUI.cs
@@ -1,17 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.UI;
+using TMPro;
 
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] LevelUiManager levelUiManager;
-    private SceneLoader scene;
+    [SerializeField] GameObject textGuideObject;
+    [SerializeField] TextMeshProUGUI textGuide;
+    [SerializeField] private string initialTutorialText;
     private AudioManager audioManager;
+
+    // Час, через який текст зникне (у секундах)
+    public float disappearanceTime = 5.0f;
+
+    
     private void Start()
     {
         audioManager = AudioManager.Instance;
-        scene = SceneLoader.Instanse;
+        ShowText(initialTutorialText);
     }
+
+    
+    // Публічний метод для відображення тексту
+    public void ShowText(string textToShow)
+    {
+        if (textGuideObject != null && textGuide != null)
+        {
+            textGuide.text = textToShow;
+            textGuideObject.SetActive(true);
+            StartCoroutine(HideTextAfterDelay());
+        }
+    }
+
+    // Корутина, що чекає певний час, а потім приховує текст
+    private IEnumerator HideTextAfterDelay()
+    {
+        yield return new WaitForSeconds(disappearanceTime);
+        textGuideObject.SetActive(false);
+    }
+
 
     public void btPause()
     {
